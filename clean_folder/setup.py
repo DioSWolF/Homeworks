c696896722e8139189fd9b75aa910850dab5200f
DioSWolF <diosvolk@gmail.com>
2022-08-13 22:25:19 +0300

    Update setup.py

--- clean_folder/setup.py
+++ clean_folder/setup.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from setuptools import setup
 
 
@@ -6,4 +7,14 @@ setup(
     author = "Dios Wolf",
     packages = ["clean_folder"],
     entry_points = {"console_scripts" : ["clean = clean_folder.clean:clean"]},
+=======
+from setuptools import setup
+
+
+setup(
+    name = "clean",
+    author = "Dios Wolf",
+    packages = ["clean_folder"],
+    entry_points = {"console_scripts" : ["clean = clean_folder.clean:clean"]},
+>>>>>>> 0046d9d1eb85ede31c5f7027a30d0080f3d38870
 )
